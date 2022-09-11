bc35a38db8658bc4387b95dc5169e147192e1996
Agustin Gomez <agustingomez1986@hotmail.com>
2022-09-11 09:53:02 -0300

    modificacion ruta serviciosCRUD en verProducto.js

--- js/botones/verProducto.js
+++ js/botones/verProducto.js
@@ -1,5 +1,5 @@
-import { serviciosCRUD } from "../servicios/serviciosCRUD.js";
 import { mostrarVentanaDescripcion } from "../verProductoVentana.js";
+import { serviciosCRUD } from "../Servicios/serviciosCRUD.js"
 
 export const verProducto = (id)=>{
    const botonVerProducto = document.createElement("button");
